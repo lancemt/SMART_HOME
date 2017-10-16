ac3008174aea3221fe2123b854df89feba80a4b8
alwarkartik <alwarkartik@gmail.com>
2017-10-16 14:52:44 +1100

    Updated working model

--- HomeMonitor/HomeMonitorDriver.py
+++ HomeMonitor/HomeMonitorDriver.py
@@ -1,13 +1,12 @@
-import uuid
 import time
+import uuid
 from SmartHome.Common.Event import Event
 from SmartHome.HomeMonitor.MqttPublisher import MqttPublisher
-#from SmartHome.HomeMonitor.Configurator import Configurator
+from SmartHome.HomeMonitor.Configurator import Configurator
 
 publisher = MqttPublisher("ABCDEF12345")
 
 # /Users/UTS/Desktop/ICT-D/SensorData.csv --> should be changed to csv file location
-
 while(1):
     r = Configurator("http://192.168.43.64/SensorData.csv")
 
@@ -16,8 +15,8 @@ while(1):
     SensorID = r.SensorID()
     eventStatus = r.Rule_Engine(r)
 
-    if (eventStatus == "None"):
-        e = Event(str(uuid.uuid4()), "dfdf", "Some Description", "12:00", "HM1", "fdfds")
+    if (eventStatus != "None"):
+        e = Event(str(uuid.uuid4()), eventStatus, "Some Description", "12:00", "HM1", str(SensorID))
         publisher.publish_event(e)
 
     # Give time for the event to be published, leave MQTT publisher open for disconnect events etc.
