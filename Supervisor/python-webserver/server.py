1d52986dfe72a86d30f90bba9a6b58c375199e8f
YousefH <yusuf.aahh@gmail.com>
2017-10-22 08:51:29 +1100

    implemented beaker's sessions manager

--- Supervisor/python-webserver/server.py
+++ Supervisor/python-webserver/server.py
@@ -2,12 +2,25 @@
 
 import threading
 from gevent import monkey; monkey.patch_all()
-from bottle import run
+import bottle
+from bottle.ext import beaker
 
 import routes # Setup Routes
+
+
+session_opts = {
+	'session.cookie_expires': True,
+	'session.encrypt_key': 'please use a random key and keep it secret!',
+	'session.httponly': True,
+	'session.timeout': 60*5,#3600 * 24,  # 1 day
+	'session.type': 'cookie',
+	'session.validate_key': True,
+}
+
+app = beaker.middleware.SessionMiddleware(bottle.app(), session_opts)
 
 # ============= #
 # SERVER STARTS #
 # ============= #
 # run(host='0.0.0.0', port=3000, debug=True)
-threading.Thread(target=run, kwargs=dict(host='0.0.0.0', port=3000, server='gevent', debug=False)).start()
+threading.Thread(target=bottle.run, kwargs=dict(app=app, host='0.0.0.0', port=3000, server='gevent', debug=False)).start()
