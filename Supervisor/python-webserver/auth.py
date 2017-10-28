6b3ac2c04e2a71dc2d38b1f9c1406cae3a845493
YousefH <yusuf.aahh@gmail.com>
2017-10-29 00:29:49 +1100

    fixed `checklogin`, and implemented `registerUser`

--- Supervisor/python-webserver/auth.py
+++ Supervisor/python-webserver/auth.py
@@ -12,27 +12,26 @@ client = pymongo.MongoClient('localhost', 27017)
 db = client['test']
 User = db['User']
 
-def checklogin(id, str):
-    user = User.find_one({"userID": id})
-    pw = base64.b64encode(hashlib.sha256(str.encode('utf-8')).digest())
-    hashed = bcrypt.hashpw(pw, bcrypt.gensalt())
-    print("input:")
-    print(pw)
-    print("Stored:")
-    spw = base64.b64encode(hashlib.sha256(user["password"].encode('utf-8')).digest())
-    print(spw)
-    if pw == spw:
+def checklogin(email, psw):
+    user = User.find_one({"userID": email})
+    pw = base64.b64encode(hashlib.sha256(psw.encode('utf-8')).digest())
+    hashed = user["password"]
+    if(bcrypt.checkpw(pw, hashed)):
         al = user["accesslevel"]
         print(hashed)
         print(al)
         return al
 
-
+def registerUser(email, psw, lvl):
+    pw = base64.b64encode(hashlib.sha256(psw.encode()).digest())
+    hashed = bcrypt.hashpw(
+        pw,
+        bcrypt.gensalt()
+    )
+    bcrypt.checkpw(pw, hashed)
+    User.insert_one({
+            "userID": email,
+            "password": hashed,
+            "accesslevel":lvl
+        })
 #password = b"an incredibly long password" * 10 #This was just experimentation code to check the bycrypt password
-#pw = base64.b64encode(hashlib.sha256(password).digest())
-#hashed = bcrypt.hashpw(
-#	pw,
-#	bcrypt.gensalt()
-#)
-
-#bcrypt.checkpw(pw, hashed)
