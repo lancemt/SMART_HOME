19398b86f7af9a12e57f4ffc7faddfa3a72d17a5
YousefH <yusuf.aahh@gmail.com>
2017-10-29 09:56:16 +1100

    clean up + added refresh capability for alarm page

--- Supervisor/python-webserver/routes.py
+++ Supervisor/python-webserver/routes.py
@@ -6,7 +6,7 @@ from bottle.ext import beaker
 from auth import checklogin
 
 from time import sleep
-from populateAlarmTable import getAlarms, createRow, updateAlarmById
+from populateAlarmTable import getAlarms, createRow, updateAlarmById, refAlarms
 
 bottle.TEMPLATE_PATH.insert(0, 'app/views/')
 
@@ -24,24 +24,27 @@ def test():
     s['bob'] = s.get('bob',0) +2
     s.save()
     return 'Test counter: %d, %d' % (s['test'], s['bob'])
+
 @route('/', method = 'get')
-
 def home_page():
-	# response.set_header('Content-type', 'image/jpeg')
 	return template('index.tpl', User='')
-@route('/alarmpage', method = 'get')
 
+@route('/alarmpage', method = 'get')
 def alarm_page():
+	s = bottle.request.environ.get('beaker.session')
 	data = ""
 	ack_data = ""
+	row = None
 	for row in getAlarms():
 		if row['acknowledged_by']:
 			ack_data += createRow(row, 1)
 		else:
 			data += createRow(row, "")
+	s['latestDate'] = row['time_of_event'].timestamp() if row != None else None
+	s.save()
 	return template('alarmPage.tpl', User='testUserEmail@email.we', data=data, ack_data=ack_data)
-@route('/login', method='post')
 
+@route('/login', method='post')
 def login():
 	username = request.forms.get('email')
 	password = request.forms.get('password')
@@ -54,12 +57,6 @@ def login():
 	else:
                return "Something"
 	
-@route('/wait', method='post')
-
-def waiting():
-	sleep(10)
-	redirect('/alarmpage')
-
 # Assets
 @route('/js/<filename>', method = 'get')
 def js(filename):
@@ -70,6 +67,30 @@ def css(filename):
 @route('/fonts/<dirName>/<filename>', method = 'get')
 def fonts(dirName, filename):
 	return static_file(filename, root='app/assets/fonts/' + dirName)
+
+@route('/refreshTables', method = 'post')
+def refresh_tables():
+	s = bottle.request.environ.get('beaker.session')
+	s['latestDate'] = s.get('latestDate', None)
+	latestDate = s['latestDate']
+	print(latestDate)
+	# latestDate = request.get_cookie("latestDate")
+	result = []
+	timeout = 0
+	while(result == []):
+		result = refAlarms(latestDate=latestDate if latestDate != "" else None)
+		sleep(0.5)
+		timeout += 0.5
+		if(timeout >= 5):
+			response.status = 299
+			return "timeout"
+	s['latestDate'] = result[-1]['time_of_event'].timestamp()
+	s.save()
+	# response.set_cookie("latestDate", latestDate)
+	data = ""
+	for row in result:
+		data += createRow(row, "") # HTML table rows to append
+	return data
 
 
 @route('/hello')
