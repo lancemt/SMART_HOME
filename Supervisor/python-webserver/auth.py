b20250920b0765c67a2c309cd0002a62fc8dce21
alwarkartik <alwarkartik@gmail.com>
2017-10-28 16:06:26 +1100

    Authentication Update for Log in

--- Supervisor/python-webserver/auth.py
+++ Supervisor/python-webserver/auth.py
@@ -1,16 +1,38 @@
 import bcrypt
 import base64
 import hashlib
+import pymongo
+import hashlib
 
 from datetime import datetime, timedelta
 from time import time
 
+client = pymongo.MongoClient()
+client = pymongo.MongoClient('localhost', 27017)
+db = client['test']
+User = db['User']
 
-password = b"an incredibly long password" * 10
-pw = base64.b64encode(hashlib.sha256(password).digest())
-hashed = bcrypt.hashpw(
-	pw,
-	bcrypt.gensalt()
-)
+def checklogin(id, str):
+    user = User.find_one({"userID": id})
+    pw = base64.b64encode(hashlib.sha256(str.encode('utf-8')).digest())
+    hashed = bcrypt.hashpw(pw, bcrypt.gensalt())
+    print("input:")
+    print(pw)
+    print("Stored:")
+    spw = base64.b64encode(hashlib.sha256(user["password"].encode('utf-8')).digest())
+    print(spw)
+    if pw == spw:
+        al = user["accesslevel"]
+        print(hashed)
+        print(al)
+        return al
 
-bcrypt.checkpw(pw, hashed)
+
+#password = b"an incredibly long password" * 10 #This was just experimentation code to check the bycrypt password
+#pw = base64.b64encode(hashlib.sha256(password).digest())
+#hashed = bcrypt.hashpw(
+#	pw,
+#	bcrypt.gensalt()
+#)
+
+#bcrypt.checkpw(pw, hashed)
